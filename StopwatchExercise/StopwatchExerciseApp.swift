133b17e61accaa8c2d9289db0f79102fb85d335f
rosqoe <rosqoe@icloud.com>
2022-11-15 14:29:10 +0100

    Initial Commit

---
+++ StopwatchExercise/StopwatchExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  StopwatchExerciseApp.swift
+//  StopwatchExercise
+//
+//  Created by Sebastien Bastide on 15/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct StopwatchExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
