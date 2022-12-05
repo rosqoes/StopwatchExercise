74c5ba5434830f174e36516e65d817eb6dc201b2
PierreMaddio <pmsymf@gmail.com>
2022-12-05 16:25:51 +0100

    test_watch

--- StopwatchExerciseTests/StopwatchExerciseTests.swift
+++ StopwatchExerciseTests/StopwatchExerciseTests.swift
@@ -6,10 +6,33 @@
 //
 
 import XCTest
+import Combine
+@testable import StopwatchExercise
+
 
 final class StopwatchExerciseTests: XCTestCase {
-
-    func test_() {
+    var timerPublisher: AnyPublisher<Publishers.Autoconnect<Timer.TimerPublisher>.Output, Publishers.Autoconnect<Timer.TimerPublisher>.Failure>!
+    var cancellable: AnyCancellable!
+    var watch: Watch!
+    
+    override func setUp() {
+        timerPublisher = Timer.publish(every: 1.0, on: .main, in: .common).autoconnect().eraseToAnyPublisher()
+        cancellable = .none
+        watch = Watch(timerPublisher: timerPublisher)
+    }
+    
+    func test_when_stop_after_5_seconds_should_print_2455() {
+        let start: () = watch.start()
+        let remainingTime = watch.remainingTime - 5
+        let pause: () = watch.pause()
+        let time = "24:55"
         
+        let promise = expectation(description: "time is 24:55")
+        
+//        watch.$time.sink { time in
+//            XCTAssertEqual(watch.$time, "24:55")
+//            promise.fulfill()
+//        }.store(in: &cancellable)
+//        wait(for: [promise], timeout: 2)
     }
 }
