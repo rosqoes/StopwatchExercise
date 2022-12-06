e95ce0cf3aac9019f5ce7d5bf6945b1d7e663396
PierreMaddio <pmsymf@gmail.com>
2022-12-06 15:21:13 +0100

    adding test init

--- StopwatchExerciseTests/StopwatchExerciseTests.swift
+++ StopwatchExerciseTests/StopwatchExerciseTests.swift
@@ -11,28 +11,39 @@ import Combine
 
 
 final class StopwatchExerciseTests: XCTestCase {
-    var timerPublisher: AnyPublisher<Publishers.Autoconnect<Timer.TimerPublisher>.Output, Publishers.Autoconnect<Timer.TimerPublisher>.Failure>!
-    var cancellable: AnyCancellable!
-    var watch: WatchViewModel!
     
-    override func setUp() {
-        timerPublisher = Timer.publish(every: 1.0, on: .main, in: .common).autoconnect().eraseToAnyPublisher()
-        cancellable = .none
-        // watch = WatchViewModel(timerPublisher: timerPublisher)
+    func test_init_startWithCorrectValue() {
+        let (sut, _) = makeSUT()
+        
+        XCTAssertEqual(sut.remainingTime, 1500)
+        XCTAssertEqual(sut.isStarted, false)
     }
     
-    func test_when_stop_after_5_seconds_should_print_2455() {
-        let start: () = watch.start()
-        let remainingTime = watch.remainingTime - 5
-        let pause: () = watch.pause()
-        let time = "24:55"
+    func test_init_start() {
+        let (sut, publisher) = makeSUT()
         
-        let promise = expectation(description: "time is 24:55")
+        sut.start()
+        (1 ... 29).forEach { _ in publisher.sendValue() }
         
-//        watch.$time.sink { time in
-//            XCTAssertEqual(watch.$time, "24:55")
-//            promise.fulfill()
-//        }.store(in: &cancellable)
-//        wait(for: [promise], timeout: 2)
+        XCTAssertEqual(sut.time, "24:30")
+        XCTAssertEqual(sut.isStarted, true)
+    }
+    
+    private func makeSUT(duration: Int = 1500) -> (sut: WatchViewModel, publisher: PublisherStub) {
+        let publisher = PublisherStub()
+        let sut = WatchViewModel(timerPublisher: publisher.valuePublisher, duration: duration)
+        return (sut, publisher)
+    }
+    
+    private class PublisherStub {
+        private let valueSubject = CurrentValueSubject<Date, Never>(.now)
+        
+        var valuePublisher: AnyPublisher<Date, Never> {
+            valueSubject.eraseToAnyPublisher()
+        }
+        
+        func sendValue() {
+            valueSubject.send(.now)
+        }
     }
 }
